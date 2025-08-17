de5658d334478752471527fedc6e1113e483fdab
xwilson03 <xanderwilson2014@gmail.com>
2025-08-17 18:41:45 -0500

    adds teleport_to signal

--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -7,5 +7,8 @@ static var instance := Globals.new()
 signal _player_pos(pos: Vector3)
 static var player_pos := Signal(instance._player_pos)
 
+signal _teleport_to(pos: Vector3)
+static var teleport_to := Signal(instance._teleport_to)
+
 signal _camera_rot(rot: Vector3)
 static var camera_rot := Signal(instance._camera_rot)
